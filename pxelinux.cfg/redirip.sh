eb627d8aa9bc0f8d1517d8cd4d3ad615f5243c15
ondrejj <ondrejj@c21728ea-8e09-0410-9ecb-e72249775932>
2012-04-05 10:30:20 +0000

    update
    git-svn-id: file:///var/svn/pxeboot@2931 c21728ea-8e09-0410-9ecb-e72249775932

--- pxelinux.cfg/redirip.sh
+++ pxelinux.cfg/redirip.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-if [ -z "$2" ]; then
-  echo "Usage: $0 IP config"
+list_ips() {
+  H='[0123456789ABCDEF]'
+  for i in $H$H$H$H$H$H$H$H; do
+    if [ -f $i ]; then
+      IPO=`echo $i | sed 's/\(..\)/0x\1 /g'`
+      IP=`printf "%d.%d.%d.%d" $IPO`
+      echo -n "$i=$IP"
+      grep ^DEFAULT $i | sed 's/^DEFAULT/:/'
+    fi
+  done
+}
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 IP [config]"
+  echo "If no config defined, configuration for this IP will be deleted."
+  list_ips
   exit 0
 fi
 
 IPO=`echo $1 | tr '.' ' '`
 FN=`printf "%02x%02x%02x%02x" $IPO | tr a-z A-Z`
 shift
+
+if [ -z "$1" ]; then
+  if [ -f "$FN" ]; then
+    rm -f $FN
+  fi
+fi
 
 cat > $FN << EOF
 INCLUDE pxelinux.cfg/fedora
