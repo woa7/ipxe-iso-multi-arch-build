a9faa9a24f82101d026012d835f5f2d5e2b176c2
ondrejj <ondrejj@c21728ea-8e09-0410-9ecb-e72249775932>
2012-04-14 20:53:26 +0000

    update
    git-svn-id: file:///var/svn/pxeboot@2940 c21728ea-8e09-0410-9ecb-e72249775932

--- images/pmagic/scripts/change-root-password.sh
+++ images/pmagic/scripts/change-root-password.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo 'root:$1$Nd.xn29E$ZyPRpRorSV06piZyARGxy/' | chpasswd -e
+eval "`cat /proc/cmdline | tr ' ' '\n' | grep -e ^password= -e ^passwd=`"
+
+if [ "$passwd" ]; then
+  password="$passwd"
+fi
+
+if [ "$password" ]; then
+  echo "Changing root's password to: $password"
+  echo "root:$password" | chpasswd
+else
+  echo 'root:$1$Nd.xn29E$ZyPRpRorSV06piZyARGxy/' | chpasswd -e
+fi
